5d4a5138d45646f2dc7c395ba49e588a13b10283
李育丞 <135806422+seanLeeYuChenge@users.noreply.github.com>
2024-11-17 12:04:19 +0800

    1117第二次更新

---
+++ lesson_9/main.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+import streamlit as st
+from time import sleep
+from tools import get_data
+load_dotenv()
+
+
+@st.dialog("目前發生問題,請稍後再試")
+def alert():
+        st.write('連線有問題')
+        st.stop()
+
+with st.spinner('下載資料中...'):
+        get_data()
+alert()
+st.write('顯示頁面')
